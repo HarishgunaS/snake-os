fc46b8a99ec1792266da1e5a70d37206dea9f392
HarishgunaS <harishguna.satgunarajah@mail.utoronto.ca>
2021-03-27 19:26:19 -0400

    stack usage

--- main.asm
+++ main.asm
@@ -1,35 +1,34 @@
-[org 0x7c00]
-
 mov ah, 0x0e
 
+mov bp, 0x8000
+mov sp, bp
 
-mov al, "1"
-int 0x10
-mov al, the_secret
-int 0x10
+push 'A'
+push 'B'
+push 'C'
 
-mov al, "2"
-int 0x10
-mov al, [the_secret]
-int 0x10
-
-mov al, "3"
-int 0x10
-mov bx, the_secret
-add bx, 0x7c00
-mov al, [bx]
+mov al, [0x7ffe]
 int 0x10
 
-mov al, "4"
-int 0x10
-mov al, [0x7c2d]
+mov al, [0x8000]
 int 0x10
 
+pop bx
+mov al, bl
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+mov al, [0x8000]
+int 0x10
 
 jmp $
-
-the_secret:
-    db "X"
 
 times 510 - ($-$$) db 0
 dw 0xaa55

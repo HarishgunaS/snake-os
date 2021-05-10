7782e10c9ff0d868cede7a6f4693cf1c409236f6
HarishgunaS <harishguna.satgunarajah@mail.utoronto.ca>
2021-05-10 16:17:44 -0400

    WASD controls for moving red square

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,66 +1,77 @@
-
 #include "../interrupts/idt.h"
 #include "../drivers/screen.h"
 #include "../drivers/keyboard.h"
 #include "../drivers/timer.h"
+
+
+
 int kernel_main()
 {
     init_timer();
     init_keyboard();
     initialize_idt();
     
-    int x = 0;
+    int x = 100;
+    int y = 100;
+    int x_velocity = 1;
+    int y_velocity = 0;
+
+
     while (1)
     {
+        unsigned char input = get_key();
+
+        square(x, y, 5, BLACK);
+
+        if (input == 0x1e)
+        {
+            //A down
+            if (x_velocity == 0)
+            {
+                x_velocity = -1;
+                y_velocity = 0;
+            }
+            
+        }
+        else if (input == 0x11)
+        {
+            //W down
+            if (y_velocity == 0)
+            {
+                y_velocity = -1;
+                x_velocity = 0;
+            }
+        }
+        else if (input == 0x20)
+        {
+            //D down
+            if (x_velocity == 0)
+            {
+                x_velocity = 1;
+                y_velocity = 0;
+            }
+            
+        }
+        else if (input == 0x1F)
+        {
+            //S down
+            if (y_velocity == 0)
+            {
+                y_velocity = 1;
+                x_velocity = 0;
+            }
+        }
+        
+        x += x_velocity;
+        y += y_velocity;
+
+        square(x, y, 5, RED);
         sleep_ms(1);
-        clear_screen(BLACK);
-        square(x++, 10, 5, RED);
     }
     // while (1)
     // {
-    //     unsigned char input = get_key();
-    //     if (input == 0x1e)
-    //     {
-    //         //A down
-            
-    //         clear_screen(RED);
-    //     }
-    //     else if (input == 0x9e)
-    //     {
-    //         //A up
-    //         clear_screen(BLACK);
-    //     }
-    //     else if (input == 0x11)
-    //     {
-    //         //W down
-    //         clear_screen(GREEN);
-
-    //     }
-    //     else if (input == 0x91)
-    //     {
-    //         //W up
-    //         clear_screen(BLACK);
-    //     }
-    //     else if (input == 0x20)
-    //     {
-    //         //D down
-    //         clear_screen(RED);
-    //     }
-    //     else if (input == 0xA0)
-    //     {
-    //         //D up
-    //         clear_screen(BLACK);
-    //     }
-    //     else if (input == 0x1F)
-    //     {
-    //         //S down
-    //         clear_screen(GREEN);
-    //     }
-    //     else if (input == 0x9f)
-    //     {
-    //         //S up
-    //         clear_screen(BLACK);
-    //     }
+    //     
+    
     // }
     for(;;)
     {
